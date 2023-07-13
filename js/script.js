12094563ea31adba42f568c8dd4c82b14d13040d
Dolores Dominguez <oranotherway@gmail.com>
2023-07-13 12:41:38 -0300

    Mejoras!

--- js/script.js
+++ js/script.js
@@ -9,11 +9,13 @@ function show() {
 
 function searchMovie() {
     var searchQuery = document.getElementById('searchInput').value;
-    const apiUrl = `http://www.omdbapi.com/?apikey=2d435d7a&t=${encodeURIComponent(searchQuery)}`;
+    const apiUrl = `http://www.omdbapi.com/?apikey=2d435d7a&t=${searchQuery}`;
     $.ajax({
         url: apiUrl,
         success: function(data) {
             if (data.Response === "True") {
+                document.getElementById('no-result').style.display = "none";
+                document.getElementById('all-movie').style.display = "block";
                 document.getElementById('movie-title').innerHTML = data.Title;
                 document.getElementById('movie-year').innerHTML = data.Year;
                 document.getElementById('movie-genre').innerHTML = data.Genre;
@@ -21,25 +23,24 @@ function searchMovie() {
                 document.getElementById('movie-actors').innerHTML = data.Actors;
                 document.getElementById('movie-plot').innerHTML = data.Plot;
                 document.getElementById('movie-poster').src = data.Poster;
-                document.getElementById('rate-movie').style.display = "block";
-
-
                 // verifica que la pelicula no tenga puntuacion
-                var movieTitle = data.Title;
+                var movieTitle = data.Title; //NO me funciona la restricción amor :( me deja poner mil puntajes
                 if (!movieRatings[movieTitle]) {
                     movieRatings[movieTitle] = [];
                 }
-
-            } else {
-                document.getElementById('movieDetails').innerHTML = 'No se encontró ninguna película con ese título.';
-                document.getElementById('rate-movie').style.display = "none";
+                else if (movieRatings[movieTitle]) {
+                    alert("Ya existe puntaje para la película");
+                }
+            }
+            else if (searchQuery === '') {
+                document.getElementById('all-movie').style.display = "none";
+                document.getElementById('no-result').style.display = "block";
             }
         },
         error: function() {
-            document.getElementById('movieDetails').innerHTML = 'Ocurrió un error al realizar la búsqueda.';
-            document.getElementById('rate-movie').style.display = "none";
-        }
-    });
+            document.getElementById('allMovie').innerHTML = 'Ocurrió un error al realizar la búsqueda.';
+    }
+  });
 }
 
 //Funcion para dar puntaje a una pelicula
